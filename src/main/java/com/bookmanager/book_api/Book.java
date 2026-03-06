189d7518dd5025d2f26d5fecc685a5f142257ca1
sakoichi <kouichi19980817@gmail.com>
2026-03-06 23:01:17 +0900

    feat: add DAO

---
+++ src/main/java/com/bookmanager/book_api/Book.java
@@ -0,0 +1,46 @@
+package com.bookmanager.book_api;
+
+public class Book {
+
+    //フィールド
+    private int id;
+    private String title;
+    private String author;
+
+    //Insert用コンストラクタ
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    //Select用コンストラクタ
+    public Book(int id, String title, String author){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    //Getter
+    public int getId(){
+        return this.id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    //Setter
+    //DBが採番するため(クライアントはセットしないため)、idのsetterは不要。
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/bookmanager/book_api/BookDAO.java
@@ -0,0 +1,25 @@
+package com.bookmanager.book_api;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDAO {
+    private JdbcTemplate jdbcTemplate;
+    
+    public BookDAO(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void setupTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, author VARCHAR(255))";
+        jdbcTemplate.execute(sql);
+    }
+
+    public boolean insert(Book book){
+        //jdbcTemplate.updateは処理結果をintで行数返してくれる
+        String sql = "INSERT INTO books (title, author) VALUES (?, ?)";
+        int rows = jdbcTemplate.update(sql, book.getTitle(), book.getAuthor());
+        return rows > 0;
+    }
+}
