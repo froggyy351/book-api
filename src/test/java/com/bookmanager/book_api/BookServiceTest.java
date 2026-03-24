0f26105d1265ded70754813266d293b08852b1ca
sakoichi <kouichi19980817@gmail.com>
2026-03-24 22:44:24 +0900

    feat: add BookServiceTest.java

---
+++ src/test/java/com/bookmanager/book_api/BookServiceTest.java
@@ -0,0 +1,43 @@
+package com.bookmanager.book_api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.bookmanager.book_api.BookDAO;
+import com.bookmanager.book_api.BookService;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+    @Mock
+    BookDAO dao;
+
+    @InjectMocks
+    BookService service;
+
+    @Test
+    void getAll_全件返すこと() {
+        //Arrange：dao.finAll()が呼ばれたら2件のリストを返すよう設定
+        List<Book> mockBooks = List.of(
+            new Book(1, "Java入門", "田中太郎"),
+            new Book(2, "SQL基礎", "山本博")
+        );
+        when(dao.findAll()).thenReturn(mockBooks);
+
+        //Act: テスト対象を実行
+        List<Book> result = service.getAll();
+
+        //Assert：結果を検証
+        assertEquals(2, result.size());
+        verify(dao, times(1)).findAll();
+    }
+}
